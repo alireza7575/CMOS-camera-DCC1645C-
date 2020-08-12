e222d6b40d3e21e114ce5a3bde30c34f224e5292
AlirezaAhmadi <41507280+alireza7575@users.noreply.github.com>
2020-08-12 18:13:32 +0800

    first commit

---
+++ DCC1645C/DCC1645C/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace DCC1645C
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.start = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.Stop = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(388, 264);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(75, 23);
+            this.start.TabIndex = 0;
+            this.start.Text = "Start";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.Capture_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(370, 275);
+            this.pictureBox.TabIndex = 1;
+            this.pictureBox.TabStop = false;
+            // 
+            // Stop
+            // 
+            this.Stop.Location = new System.Drawing.Point(388, 235);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(75, 23);
+            this.Stop.TabIndex = 0;
+            this.Stop.Text = "stop";
+            this.Stop.UseVisualStyleBackColor = true;
+            this.Stop.Click += new System.EventHandler(this.Stop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(470, 304);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.Stop);
+            this.Controls.Add(this.start);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button Stop;
+    }
+}
+
---
+++ DCC1645C/DCC1645C/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DCC1645C
+{
+    public partial class Form1 : Form
+    {   
+        
+        CameraDriver _cameraDriver;
+        public Form1()
+        {
+            InitializeComponent();
+            _cameraDriver = new CameraDriver(this.Handle.ToInt64());
+            _cameraDriver.Init(pictureBox.Handle.ToInt64());
+            _cameraDriver.CameraCapture += _cameraDriver_CameraCapture;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        [System.Security.Permissions.PermissionSet(System.Security.Permissions.SecurityAction.Demand, Name = "FullTrust")]
+        protected override void WndProc(ref Message m)
+        {
+            // Listen for operating system messages
+            if (_cameraDriver != null)
+            {
+                switch (m.Msg)
+                {
+                    case uc480.IS_UC480_MESSAGE:
+                        _cameraDriver.HandleMessage(m.Msg, m.LParam.ToInt64(), m.WParam.ToInt32());
+                        break;
+                }
+
+            }
+            base.WndProc(ref m);
+        }
+        void _cameraDriver_CameraCapture(object sender, Bitmap b)
+        {
+            pictureBox.Image = b;
+        }
+        private void Capture_Click(object sender, EventArgs e)
+        {
+            _cameraDriver.StartVideo();
+        }
+
+        private void Stop_Click(object sender, EventArgs e)
+        {
+            _cameraDriver.StopVideo();
+        }
+    }
+}
